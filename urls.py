6379d89e53ad2901020af81f7ef5ee9befcc764d
phillc <phillc@a0317602-fb9a-4f44-8258-54b326434a0c>
2008-02-27 03:27:28 +0000

    git-svn-id: http://svn.assembla.com/svn/phillc/wtflab/trunk@10 a0317602-fb9a-4f44-8258-54b326434a0c

--- urls.py
+++ urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls.defaults import *
+from wtflab.apps.blog.models import Entry
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^wtflab/', include('wtflab.foo.urls')),
-
-    # Uncomment this for admin:
-#     (r'^admin/', include('django.contrib.admin.urls')),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'C:/Users/phillc/Desktop/workspace/wtflab/htdocs/media/'}),
+    (r'^admin/', include('django.contrib.admin.urls')),
+    (r'', include('wtflab.apps.blog.urls')),
 )
