70076f96fc15d721908d6e013a6265029ebd62d2
phillc <phillc@a0317602-fb9a-4f44-8258-54b326434a0c>
2008-02-29 04:52:46 +0000

    git-svn-id: http://svn.assembla.com/svn/phillc/wtflab/trunk@35 a0317602-fb9a-4f44-8258-54b326434a0c

--- wtflab/apps/blog/urls.py
+++ wtflab/apps/blog/urls.py
@@ -8,16 +8,15 @@ info_dict = {
 
 #show entire blog by date
 urlpatterns = patterns('django.views.generic.date_based',
-    (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', 'archive_day', info_dict),
     (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', 'archive_month', info_dict),
     (r'^(?P<year>\d{4})/$', 'archive_year', info_dict),
-    (r'^[ ]{0}$', 'archive_index', info_dict),
+    (r'^[ ]{0}$', 'archive_index', dict(info_dict, template_object_name='object_list')),
 )
 
 #break down to sections
 urlpatterns += patterns('wtflab.apps.blog.views',
     (r'^(?P<section>[\w-]+)/$', 'section'),
-    (r'^(?P<section>[\w-]+)/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>\w+)/$', 'entry_detail'),
+    (r'^(?P<section>[\w-]+)/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>\w+)/$', 'archive_day'),
     (r'^(?P<section>[\w-]+)/(?P<year>\d{4})/(?P<month>[a-z]{3})/$', 'archive_month'),
     (r'^(?P<section>[\w-]+)/(?P<year>\d{4})/$', 'archive_year'),
 )
--- wtflab/apps/blog/views.py
+++ wtflab/apps/blog/views.py
@@ -2,15 +2,15 @@ from models import Entry, Section
 from django.shortcuts import get_object_or_404
 from django.views.generic import date_based
 
-def sections_index():
-    return true
-
 def section(request, section):
     sectiono = get_object_or_404(Section, slug=section)
     return date_based.archive_index(
         request,
         date_field = 'publish',
         queryset = Entry.published.filter(section=sectiono),
+        template_name = 'blog/section_archive.html',
+        template_object_name = 'object_list',
+        extra_context = {'section': sectiono.title},
     )
 
 def archive_year(request, section, year):
@@ -20,6 +20,8 @@ def archive_year(request, section, year):
         year = year,
         date_field = 'publish',
         queryset = Entry.published.filter(section=sectiono),
+        template_name = 'blog/section_archive_year.html',
+        extra_context = {'section': sectiono.title},
     )
 
 def archive_month(request, section, year, month):
@@ -30,12 +32,11 @@ def archive_month(request, section, year, month):
         month = month,
         date_field = 'publish',
         queryset = Entry.published.filter(section=sectiono),
+        template_name = 'blog/section_archive_month.html',
+        extra_context = {'section': sectiono.title},
     )
 
 def archive_day():
-    return true
-
-def entry_detail():
     return true
 
 
