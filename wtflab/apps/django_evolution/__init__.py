1c0e2182779430dcbf5d7a021cc1d805192e7a75
phillc <phillc@a0317602-fb9a-4f44-8258-54b326434a0c>
2008-03-05 04:08:54 +0000

    git-svn-id: http://svn.assembla.com/svn/phillc/wtflab/trunk@54 a0317602-fb9a-4f44-8258-54b326434a0c

---
+++ wtflab/apps/django_evolution/__init__.py
@@ -0,0 +1,12 @@
+class EvolutionException(Exception):
+    def __init__(self,msg):
+        self.msg = msg
+
+    def __str__(self):
+        return str(self.msg)
+        
+class CannotSimulate(EvolutionException):
+    pass
+    
+class SimulationFailure(EvolutionException):
+    pass
