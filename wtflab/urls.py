11afe5b2fb00bc15e2647bc94f0a47c9f045646e
phillc <phillc@a0317602-fb9a-4f44-8258-54b326434a0c>
2008-03-10 02:49:41 +0000

    git-svn-id: http://svn.assembla.com/svn/phillc/wtflab/trunk@64 a0317602-fb9a-4f44-8258-54b326434a0c

--- wtflab/urls.py
+++ wtflab/urls.py
@@ -4,6 +4,6 @@ urlpatterns = patterns('',
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'C:/Users/phillc/Desktop/workspace/wtflab.com/media/'}),
     (r'^admin/', include('django.contrib.admin.urls')),
     (r'^comments/', include('django.contrib.comments.urls.comments')),
-    (r'', include('blog.urls')),
     (r'^google9eee2da00290540a.html$', 'blog.views.section'),
+    (r'', include('blog.urls')),
 )
