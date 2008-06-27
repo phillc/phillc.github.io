62580b0cbb7e7d51cc0c700186bdbe6314d1d9c9
phillc <phillc@a0317602-fb9a-4f44-8258-54b326434a0c>
2008-06-27 02:22:16 +0000

    git-svn-id: http://svn.assembla.com/svn/phillc/wtflab/trunk@148 a0317602-fb9a-4f44-8258-54b326434a0c

--- urls.py
+++ urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls.defaults import *
+from beerthree.settings import DEBUG
 
 urlpatterns = patterns('',
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'C:/Users/phillc/Desktop/workspace/wtflab.com/media/'}),
     (r'^admin/', include('django.contrib.admin.urls')),
     (r'^comments/', include('django.contrib.comments.urls.comments')),
     (r'^google9eee2da00290540a.html$', 'blog.views.section', {'section': 'wtf'}),
     (r'', include('blog.urls')),
 )
+
+if DEBUG:
+    from settings import MEDIA_ROOT
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
+    )
