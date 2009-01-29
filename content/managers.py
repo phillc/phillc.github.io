175be9722ae0ac8482144d8ffcc34a6641e5380c
Phillip <spyyderz@gmail.com>
2009-01-29 17:26:01 -0500

    These are being moved to phillc-blog

--- content/managers.py
+++
@@ -1,7 +0,0 @@
-from django.db.models import Manager
-from datetime import datetime
-
-class PublishedManager(Manager):
-    def get_query_set(self):
-        queryset = super(PublishedManager, self).get_query_set()
-        return queryset.filter(status=2, publish__lte=datetime.now)
