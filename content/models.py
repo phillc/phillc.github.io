de9dd7c267a677100abc25668d46f879b9089e66
Phillip <spyyderz@gmail.com>
2009-02-04 20:07:08 -0500

    move stuff

--- content/models.py
+++ content/models.py
@@ -3,9 +3,7 @@ from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import permalink
-
-from tagging.fields import TagField
-
+from django.utils.translation import ugettext_lazy as _
 
 class Section(models.Model):
     """This represents a Section of the website"""
@@ -16,25 +14,26 @@ class Section(models.Model):
         return '%s' % self.title
     
 class Content(models.Model):
-    STATUS_CHOICES = (
-        (1, 'Draft'),
-        (2, 'Public'),
-        (3, 'Closed'),
-    )
 
     created_by = models.ForeignKey(User)
 
     title = models.CharField(_('title'), max_length=64)
     slug = models.SlugField(_('slug'), unique_for_month=True)
 
-    author           = models.ForeignKey(Author)
+    section = models.ForeignKey(Section)
 
-    status           = models.IntegerField(choices=STATUS_CHOICES, radio_admin=True, default=1)
-    publish          = models.DateTimeField(default=datetime.now)
-    created          = models.DateTimeField(editable=False)
-    modified         = models.DateTimeField(editable=False)
-    section          = models.ForeignKey(Section)
-    
+    author = models.ForeignKey(Author)
+
+    created = models.DateTimeField(editable=False)
+    shown_date = models.DateTimeField(default=datetime.datetime.now)
+    modified = models.DateTimeField(editable=False)
+
+    is_published = models.BooleanField(default=True)
+    publish_on = models.DateTimeField(default=datetime.now)
+    publish_end = models.DateTimeField(blank=True, null=True)
+
+    objects = ContentManager()
+
     def __unicode__(self):
         return '%s' % self.title
 
@@ -44,16 +43,4 @@ class Content(models.Model):
 	else:
             self.modified = datetime.datetime.now()
 
-
-    @permalink
-    def get_absolute_url(self):
-        return ('blog_entry', (),
-            {
-                'sectionSlug' : str(self.section.slug),
-                'year'        : str(self.publish.year),
-                'month'       : str(self.publish.strftime('%b')).lower(),
-                'day'         : str(self.publish.day).zfill(2),
-                'slug'        : str(self.slug),
-            },
-        )
-        
+ 
