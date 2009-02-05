b3936016c4db5c948b43b2e4bd9b2418a0015fef
Phillip <spyyderz@gmail.com>
2009-02-04 19:49:04 -0500

    remove some older style stuff

--- content/models.py
+++ content/models.py
@@ -1,13 +1,8 @@
 from datetime import datetime
-from time import strftime
 
 from django.db import models
-from django.contrib import admin
 from django.contrib.auth.models import User
 from django.db.models import permalink
-from django.core.urlresolvers import reverse
-
-from blog.managers import PublishedManager
 
 from tagging.fields import TagField
 
@@ -20,7 +15,7 @@ class Section(models.Model):
     def __unicode__(self):
         return '%s' % self.title
     
-class Author(models.Model):
+class Content(models.Model):
     STATUS_CHOICES = (
         (1, 'Draft'),
         (2, 'Public'),
@@ -39,42 +34,10 @@ class Author(models.Model):
     created          = models.DateTimeField(auto_now_add=True)
     modified         = models.DateTimeField(auto_now=True)
     section          = models.ForeignKey(Section)
-    tags             = TagField()
     
-    class Meta:
-        verbose_name_plural = "Entries"
-        ordering = ['-publish']
-        
     def __unicode__(self):
         return '%s' % self.title
-    
-    def save(self):
-        if self.render_method == 'markdown':
-            import markdown
-            renderer = markdown.markdown
-        elif self.render_method == 'textile':
-            import textile
-            renderer = textile.textile
-        self.tease = renderer(self.tease_markdown)
-        self.body  = renderer(self.body_markdown)
-        super(Entry, self).save()
 
-    def get_author_display_name(self):
-        name = ''
-        try:
-            author = self.author
-        except User.DoesNotExist:
-            return False
-        if author.first_name and author.last_name:
-            name = '%s %s' % (author.first_name, author.last_name)
-        elif author.first_name:
-            name = '%s' % (author.first_name)
-        else:
-            name = author.username
-        return name
-        
-
-    
     @permalink
     def get_absolute_url(self):
         return ('blog_entry', (),
