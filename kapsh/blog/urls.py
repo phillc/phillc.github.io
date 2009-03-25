0cc319daf96fb1c790f439b7e40b42626f347606
Phillip <spyyderz@gmail.com>
2009-03-25 19:11:16 -0400

    uhhh, urls syntax from old blog...

--- kapsh/blog/urls.py
+++ kapsh/blog/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
 from kapsh.blog.models import Entry
 
-urlpatterns = patterns('blog.views',
+urlpatterns = patterns('',
     url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>[\d]{2})/(?P<slug>[\w-]+)/$',
-        view = 'entry_detail',
+        view = 'kapsh.blog.views.entry_detail',
         name='entry_detail',
     ),
 )
