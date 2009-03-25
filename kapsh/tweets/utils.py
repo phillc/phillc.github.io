307eec6951f9133015d635d31944ad8ecbb5b5d7
Phillip <spyyderz@gmail.com>
2009-03-25 00:06:00 -0400

    Heh, time zones

--- kapsh/tweets/utils.py
+++ kapsh/tweets/utils.py
@@ -2,13 +2,18 @@ import datetime
 import twitter
 from django.conf import settings
 from kapsh.tweets.models import Tweet
+import pytz
 
 def get_tweets():
+    tz = pytz.timezone('US/Eastern')
+    utc = pytz.timezone('UTC')
+
     api = twitter.Api()
     tweet_list = api.GetUserTimeline(settings.TWITTER_USER)
 
     for tweet in tweet_list:
-	tweet.date = datetime.datetime.strptime(tweet.created_at, "%a %b %d %H:%M:%S +0000 %Y")
+        date_utc = datetime.datetime.strptime(tweet.created_at, '%a %b %d %H:%M:%S +0000 %Y').replace(tzinfo=utc)
+        tweet.date = date_utc.astimezone(tz).replace(tzinfo=None)
         Tweet.objects.get_or_create(
             uid = tweet.id,
     	    defaults = {
