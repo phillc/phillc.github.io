2adb69862d990b0b52aa8db51347013d461ff064
Phillip <spyyderz@gmail.com>
2009-03-24 21:07:20 -0400

    Bunch of template moves for my sake

--- kapsh/blog/urls.py
+++ kapsh/blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from kapsh.blog.models import Entry
 
 urlpatterns = patterns('blog.views',
-    url(r'^/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>[\d]{2})/(?P<slug>[\w-]+)/$',
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>[\d]{2})/(?P<slug>[\w-]+)/$',
         view = 'entry_detail',
         name='entry_detail',
     ),
