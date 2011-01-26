fe1b25291da46aba50c7bd5c7cc193a2c504c3fe
phillc <spyyderz@gmail.com>
2011-01-25 23:10:03 -0500

    Looks like I am giving jekyll one more chance

--- lib/default.rb
+++
@@ -1,5 +0,0 @@
-# All files in the 'lib' directory will be loaded
-# before nanoc starts compiling.
-include Nanoc3::Helpers::LinkTo
-include Nanoc3::Helpers::Blogging
-include Nanoc3::Helpers::Rendering
